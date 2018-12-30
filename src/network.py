7c89dbdd18fe01489f897a6355c9a160725e7121
sushobhan sen <icecoolsushobhan@gmail.com>
2018-12-30 17:04:55 -0600

    Created an SDG member function, need to add more functions

--- src/network.py
+++ src/network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Network:
 
@@ -19,6 +20,43 @@ class Network:
 		self.sizes = sizes #number of neurons per layer (including input and output layers)
 		self.biases = [np.random.randn(y,1) for y in sizes[1:]] #biases of each neuron, except in the first layer
 		self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1],sizes[1:])] #weights between previous x and current y layer of neurons (note it's indexed as (y,x))
+		
+	def feedforward(self, a):
+		'''
+		Takes an input a and returns an output from the sigmoid function after running through the entire network
+		Note that this function rewrites a copy of the input a internally
+		'''
+		for b, w in zip(self.biases,self.weights):
+			a = sigmoid(np.dot(w,a)+b)
+		
+		return a
+		
+	def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+		'''
+		Performs a stochastic gradient descent on the training data to tune the weights and biases
+		A mini-batch SGD is used to sample the data to tune it, and this is done for several epochs
+		training_data is a tuple (x,y) of inputs and desired outputs
+		eta is the gradient descent step size
+		If test_data is provided, then the function analyzed the test data and prints output
+		'''
+		
+		if test_data: n_test = len(test_data)
+		
+		n = len(training_data)
+		
+		for j in range(epochs):
+			random.shuffle(training_data) #shuffle the training data 
+			
+			#Create mini-batches by sampling the shuffled training data
+			mini_batches = [training_data[k:k+mini_batch_size] for k in range(0,n,mini_batch_size)]
+			
+			for mini_batch in mini-batches:
+				self.update_mini_batch(mini_batch, eta) #Gradient descent on the mini-batch
+			
+			if test_data:
+				print('Epoch ',j,': ',self.evaluate(test_data),' / ',n_test)
+			else:
+				print('Epoch ',j,' complete')
 
 #Define sigmoid function
 def sigmoid(z):
